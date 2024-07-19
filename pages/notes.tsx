74f58c1c2b0c050c98bbbbacf62b6f75514b856f
bennett-taniguchi <tanigb@uw.edu>
2024-07-19 15:01:10 -0700

    added resizable panes in notes, and mock buttons, title, and body TextAreas

--- pages/notes.tsx
+++ pages/notes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GetServerSideProps } from "next";
 import { useSession, getSession } from "next-auth/react";
 import Layout from "../components/Layout";
@@ -8,6 +8,7 @@ import prisma from "../lib/prisma";
 import { ScrollArea } from "../components/ui/scroll-area";
 import { Separator } from "../components/ui/separator";
 import { Button } from "../components/ui/button";
+
 import {
   Command,
   CommandEmpty,
@@ -18,12 +19,24 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from "../components/ui/command";
+
 import { Textarea } from "../components/ui/textarea";
+
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "../components/ui/resizable";
+
+import {
+  FontBoldIcon,
+  FontItalicIcon,
+  UnderlineIcon,
+  PaperPlaneIcon,
+  Cross1Icon,
+} from "@radix-ui/react-icons";
+
+import { ToggleGroup, ToggleGroupItem } from "../components/ui/toggle-group";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = await getSession({ req });
@@ -52,9 +65,18 @@ type Props = {
   drafts: PostProps[];
 };
 
-const testNotes = ["Today", "Yesterday", "The day before"];
 const Drafts: React.FC<Props> = (props) => {
   const { data: session } = useSession();
+
+  const testNotes = ["Today", "Yesterday", "The day before"]; // to be filled with db title of notes
+  const [selected, setSelected] = useState<boolean[]>(Array(5).fill(false)); // state of button press
+  const [title, setTitle] = useState<string>();
+  const [body, setBody] = useState<string>();
+
+  function handleSelected(num: number) {
+    selected[num] = !selected[num];
+    console.log(selected);
+  }
 
   if (!session) {
     return (
@@ -69,7 +91,10 @@ const Drafts: React.FC<Props> = (props) => {
     <Layout>
       <div className="page ">
         <ResizablePanelGroup direction="horizontal">
-          <ResizablePanel className="min-h-screen min-w-[250px] rounded-lg border">
+          <ResizablePanel
+            defaultSize={20}
+            className="min-h-screen min-w-[250px] rounded-lg border"
+          >
             <ScrollArea className="rounded-md border">
               <Command className="h-100 rounded-lg border shadow-md overflow-hidden">
                 <CommandInput placeholder="Type a command or search..." />
@@ -116,7 +141,74 @@ const Drafts: React.FC<Props> = (props) => {
           </ResizablePanel>
           <ResizableHandle withHandle />
           <ResizablePanel>
-            <Textarea className="min-h-screen"></Textarea>
+            <ResizablePanelGroup direction="vertical">
+              <ResizablePanel
+                defaultSize={10}
+                className="max-h-[200px] min-h-[100px]  border-0"
+              >
+                <ToggleGroup type="multiple">
+                  <ToggleGroupItem
+                    variant="outline"
+                    value="bold"
+                    aria-label="Toggle bold"
+                    className="[date-state]-1"
+                    onClick={() => handleSelected(0)}
+                  >
+                    <FontBoldIcon className="h-4 w-4" />
+                  </ToggleGroupItem>
+                  <ToggleGroupItem
+                    variant="outline"
+                    value="italic"
+                    aria-label="Toggle italic"
+                    onClick={() => handleSelected(1)}
+                  >
+                    <FontItalicIcon className="h-4 w-4" />
+                  </ToggleGroupItem>
+                  <ToggleGroupItem
+                    variant="outline"
+                    value="strikethrough"
+                    aria-label="Toggle strikethrough"
+                    onClick={() => handleSelected(2)}
+                  >
+                    <UnderlineIcon className="h-4 w-4" />
+                  </ToggleGroupItem>
+                  <Button
+                    variant="outline"
+                    value="paperplane"
+                    aria-label="Toggle paperplane"
+                    onClick={() => handleSelected(3)}
+                    className=""
+                  >
+                    <PaperPlaneIcon className="h-4 w-4" />
+                  </Button>
+
+                  <Button
+                    variant="outline"
+                    value="cross"
+                    aria-label="toggle cross"
+                    onClick={() => handleSelected(4)}
+                    className="active:bg-zinc-400"
+                  >
+                    <Cross1Icon className="h-4 w-4" />
+                  </Button>
+                </ToggleGroup>
+              </ResizablePanel>
+              <ResizablePanel
+                defaultSize={10}
+                className="max-h-[75px] min-h-[75px] border-0"
+              >
+                <Textarea
+                  onChange={(e) => setTitle(e.target.value)}
+                  className="max-h-[75px] min-h-[75px] text-2xl resize-none  focus-visible:ring-0"
+                ></Textarea>
+              </ResizablePanel>
+              <ResizablePanel>
+                <Textarea
+                  onChange={(e) => setBody(e.target.value)}
+                  className="min-h-screen resize-none focus-visible:ring-0 "
+                ></Textarea>
+              </ResizablePanel>
+            </ResizablePanelGroup>
           </ResizablePanel>
         </ResizablePanelGroup>
       </div>
