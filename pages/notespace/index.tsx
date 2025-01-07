3facdb6042a5fd04d9c6170a1f1e3ca60d9bb621
bennett-taniguchi <tanigb@uw.edu>
2025-01-06 22:05:24 -0800

    switch between card and list

--- pages/notespace/index.tsx
+++ pages/notespace/index.tsx
@@ -20,52 +20,13 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "../../components/ui/tooltip";
+import { Card, CardContent, CardDescription, CardFooter, CardTitle } from "../../components/ui/card";
+import { useEffect, useReducer, useState } from "react";
+import { cn } from "../../components/lib/utils";
 
-export default function Notespaces() {
-  const Router = useRouter();
-  const data = [
-    {
-      title: "Math Equations",
-      sources: 3,
-      createdOn: "1/6/2025",
-      ownedBy: "Me",
-    },
-  ];
-  return (
-    <div className="w-[100svw] h-[100svh] bg-white  ">
-      <h1 className="text-7xl text-sky-600 ml-[10svw]">Notespaces</h1>
-      <Separator className="mx-auto w-[80svw] my-[2svh] py-[.5svh]" />
-      <div className=" place-items-end w-[80svw] mx-auto ">
-        <div className="flex flex-row-2 gap-2">
-
-          <TooltipProvider>
-            <Tooltip delayDuration={0}>
-              <TooltipTrigger asChild>
-                <div className="cursor-pointer">
-                  <HiViewList className="basis-1/3 h-[3svh] w-[2svw] hover:bg-zinc-200 rounded-lg" />
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>List View</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-
-          <TooltipProvider>
-            <Tooltip delayDuration={0}>
-              <TooltipTrigger asChild>
-                <div className="cursor-pointer">
-                  <PiCardsFill className="basis-1/3 h-[3svh] w-[2svw] hover:bg-zinc-200 rounded-lg  " />
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>Card View</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </div>
-      </div>
-      <Table className="w-[80svw] mx-auto">
+function TableView({data,Router}) {
+    return (
+<Table className="w-[80svw] mx-auto">
         <TableCaption>Your recent Notespaces</TableCaption>
         <TableHeader  >
           <TableRow>
@@ -114,6 +75,93 @@ export default function Notespaces() {
         </TableBody>
       </Table>
 
+    )
+}
+
+function CardView({data,Router}) {
+return(
+    <div className="w-[80svw] mx-auto">
+        {data.map((datum) => (
+            <Card onClick={()=>Router.push('/notespace/'+datum.title)} className="cursor-pointer w-[25svw] h-[20svh] bg-gradient-to-r from-cyan-500 to-white opacity-85 hover:opacity-100 ">
+                <CardTitle className="ml-[2svw] mt-[1svh]">
+                    {datum.title}
+                </CardTitle>
+                <CardDescription  className="ml-[2svw] text-slate-800">
+                    Amount of Sources: {datum.sources}
+                </CardDescription>
+                <CardContent>
+                    
+                </CardContent>
+                <CardFooter className="text-sm text-slate-800 ml-[.75svw] ">
+                    Created on {datum.createdOn}
+                </CardFooter>
+            </Card>
+        ))}
+        </div>
+)
+}
+
+function viewReducer(views,action) {
+    switch(action.type) {
+        case 'card':
+            return {view:'card'}
+        case 'list':
+            return {view:'list'}
+    }
+}
+export default function Notespaces() {
+  const Router = useRouter();
+  const initialView = {view:'list'};
+const [view,dispatch] = useReducer(viewReducer, initialView)
+  const data = [
+    {
+      title: "Math Equations",
+      sources: 3,
+      createdOn: "1/6/2025",
+      ownedBy: "Me",
+    },
+  ];
+ 
+  return (
+    <div className="w-[100svw] h-[100svh] bg-white mt-[20svh] ">
+      <h1 className="text-7xl text-sky-600 ml-[10svw]">Notespaces</h1>
+      <Separator className="mx-auto w-[80svw] my-[2svh] py-[.5svh]" />
+      <div className=" place-items-end w-[80svw] mx-auto ">
+        <div className="flex flex-row-2 gap-2">
+
+          <TooltipProvider>
+            <Tooltip delayDuration={0}>
+              <TooltipTrigger asChild>
+                <div className="cursor-pointer" onClick={(e)=>dispatch({type:'list'})}>
+                  <HiViewList className="basis-1/3 h-[3svh] w-[2svw] hover:bg-zinc-200 rounded-lg" />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>List View</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <TooltipProvider>
+            <Tooltip delayDuration={0}>
+              <TooltipTrigger asChild>
+                <div className="cursor-pointer"  onClick={(e)=>dispatch({type:'card'})}>
+                  <PiCardsFill className="basis-1/3 h-[3svh] w-[2svw] hover:bg-zinc-200 rounded-lg  " />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Card View</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      </div>
+      {view!.view =='list' ?
+      <TableView Router={Router} data={data} />
+      :
+      <CardView Router={Router} data={data}/>
+    }
+      
       <Button className="ml-[10svw] mt-[1.5svh]" variant={"outline"}>
         Create New
       </Button>
